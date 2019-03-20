69dffe223884f33b48059e8d0c8a134e5db6953e
Wenlei Dai <resca.wen@gmail.com>
2019-03-20 18:33:24 +0200

    editing should now work

--- application/tour/views.py
+++ application/tour/views.py
@@ -21,6 +21,11 @@ def tour_new():
 def tournament(id):
     return render_template("tour/tournament.html", id=id, tournament = Tournament.query.get(id))
 
+@app.route("/tournament/<string:id>/edit")
+@login_required
+def tour_edit_page(id):
+    return render_template("tour/editform.html", id=id, tournament = Tournament.query.get(id), form = TournamentForm())
+
 @app.route("/tour/create", methods=["POST"])
 @login_required
 def tour_create():
@@ -32,3 +37,17 @@ def tour_create():
     db.session().commit()
 
     return redirect(url_for("index"))  
+
+@app.route("/tournament/<string:id>/edit", methods=["POST"])
+@login_required
+def tour_edit(id):
+    form = TournamentForm(request.form)
+
+    T = Tournament.query.get(id)
+
+    T.name = form.name.data
+    T.playercount = form.playercount.data # this line maybe never even happening
+
+    db.session().commit()
+
+    return redirect(url_for('tournament', id=id))  
