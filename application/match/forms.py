c0e073761afc10c4e7200043da6e4ba3a9c4c0cd
Wenlei Dai <resca.wen@gmail.com>
2019-04-10 12:15:45 +0300

    walking skeleton/vertical integration finally complete. Matches can be submitted now

--- application/match/forms.py
+++ application/match/forms.py
@@ -1,12 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, validators
+from wtforms import StringField, IntegerField, RadioField, BooleanField, validators
 from flask_wtf.html5 import NumberInput
 
   
 class MatchForm(FlaskForm):
-    player1_score = IntegerField("Player1 Score", [validators.Length(min=4)], widget=NumberInput())
-    player2_score = IntegerField("Player2 Score", [validators.NumberRange(min=2, max=1028)], widget=NumberInput())
-    #winner_id = IntegerField("Winner", [validators.NumberRange(min=2, max=1028)])
+    player1_score = IntegerField("Player1 Score", [validators.Length(min=0, max=99)], widget=NumberInput())
+    player2_score = IntegerField("Player2 Score", [validators.NumberRange(min=0, max=99)], widget=NumberInput())
+    winner_boolean = RadioField("Winner", choices=[('player1','top win'),('player2','bottom win')])
+    player1_id = IntegerField("Player1")
+    player2_id = IntegerField("Player2")
+    
   
     class Meta:
         csrf = False
--- application/match/views.py
+++ application/match/views.py
@@ -18,8 +18,14 @@ def match_submit(id, tournament_id, match_id):
 
     editM.player1_score = form.player1_score.data
     editM.player2_score = form.player2_score.data
-    # editM.winner_id = form.winner_id.data
+    
+    if form.winner_boolean == 'player1':
+        editM.winner_id = form.player1_id.data
+    else:
+        editM.winner_id = form.player2_id.data
 
     db.session().commit()
+
+    # call Match.Winner() where we move the winner of the match to the next match
 
     return redirect(url_for('tournament', id=tournament_id))  
